88b8045c0a9a4e0a3d4f0f32ef2afea444991357
Paulo Roberto Silva <paulo.pinda@gmail.com>
2016-06-25 01:37:03 -0300

    add courses app and core

---
+++ simplemooc/core/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from simplemooc.core import views as core_views
+
+urlpatterns = [
+    url(r'^$', core_views.home, name='home'),
+    url(r'^contato/$', core_views.contact, name='contact'),
+]
---
+++ simplemooc/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+
+
+urlpatterns = [
+    url(r'^', include('simplemooc.core.urls', namespace='core')),
+    url(r'^admin/', admin.site.urls),
+]

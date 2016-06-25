024a78deb99338c3c61c70bb6e732c549d3db19a
Paulo Roberto Silva <paulo.pinda@gmail.com>
2016-06-25 01:44:30 -0300

    add admin panel

--- simplemooc/courses/admin.py
+++ simplemooc/courses/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Course
+
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'start_date', 'created_at']
+    search_fields = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Course, CourseAdmin)
--- simplemooc/courses/models.py
+++ simplemooc/courses/models.py
@@ -25,3 +25,11 @@ class Course(models.Model):
     updated_at = models.DateTimeField('Atualizado em', auto_now=True)
 
     objects = CourseManager()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Curso'
+        verbose_name_plural = 'Cursos'
+        ordering = ['name']

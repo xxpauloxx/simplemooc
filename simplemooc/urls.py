1905a865cdb40830aea169bc9263ce47867b0a69
Paulo Roberto Silva <paulo.pinda@gmail.com>
2016-06-25 23:01:28 -0300

    correction of the media in settings

--- simplemooc/urls.py
+++ simplemooc/urls.py
@@ -1,9 +1,16 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     url(r'^', include('simplemooc.core.urls', namespace='core')),
     url(r'^cursos/', include('simplemooc.courses.urls', namespace='courses')),
     url(r'^admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

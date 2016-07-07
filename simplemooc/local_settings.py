cb27e358d779780bbfd81c00e1cdd3be30eb16cc
Paulo Roberto Silva <paulo.pinda@gmail.com>
2016-07-07 19:09:04 -0300

    fix

---
+++ simplemooc/local_settings.py
@@ -0,0 +1,14 @@
+import os
+
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
